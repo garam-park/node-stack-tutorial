bb94efc2d33bd91add500a951d8c604f1b05b259
garam <netgarng88@gmail.com>
2016-05-27 18:37:56 +0900

    edited print text

--- components/Hello.js
+++ components/Hello.js
@@ -3,6 +3,6 @@ import React from 'react';
 export default class Hello extends React.Component {
     render() {
       // return React.createElement("h3", null, 'Hello Webpack!!');
-      return <h3>Hello Seperation!!</h3>
+      return <h3>Hello NO more public!!</h3>
     }
 };
