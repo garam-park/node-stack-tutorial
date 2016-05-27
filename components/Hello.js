55e75e6297f7d5a2fdee662a602d60fb18fd8e0c
garam <netgarng88@gmail.com>
2016-05-27 18:28:36 +0900

    no more public

--- components/Hello.js
+++ components/Hello.js
@@ -3,6 +3,6 @@ import React from 'react';
 export default class Hello extends React.Component {
     render() {
       // return React.createElement("h3", null, 'Hello Webpack!!');
-      return <h3>Hello No more public!!</h3>
+      return <h3>Hello Seperation!!</h3>
     }
 };
