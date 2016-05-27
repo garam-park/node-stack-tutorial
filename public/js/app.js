0cc53d739ce392766ce093894bf1f8194495c97a
garam <netgarng88@gmail.com>
2016-05-27 18:24:13 +0900

    seperation

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-class Hello extends React.Component {
-    render() {
-      // return React.createElement("h3", null, 'Hello Webpack!!');
-      return <h3>Hello ES6!!</h3>
-    }
-};
+import Hello from './components/Hello';
 
 // ReactDOM.render(React.createElement(Hello),document.getElementById('react'));
 ReactDOM.render(<Hello/>,document.getElementById('react'));
