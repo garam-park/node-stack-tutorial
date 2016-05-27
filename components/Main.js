a594cfdb43476deef8631986fff7756cc0d89f9a
garam <netgarng88@gmail.com>
2016-05-27 20:31:01 +0900

    Main component

---
+++ components/Main.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default class Main extends React.Component {
+
+  componentWillMount(){}
+  componentDidMount(){}
+
+  render() {
+    return (
+      <div>
+        <h3>Links</h3>
+        <ul>
+          <li>Link..</li>
+          <li>Link..</li>
+          <li>Link..</li>
+        </ul>
+      </div>
+    )
+  }
+};
