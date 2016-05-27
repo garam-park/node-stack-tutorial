948dbf1e4bd05d7fc9a14a8704ccbe48f33a39c1
garam <netgarng88@gmail.com>
2016-05-27 19:33:26 +0900

    mongodb test

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 import express from 'express'
+import { MongoClient } from 'mongodb'
 
 let app = express()
-
+let MONGO_URI = '';
 // app.get('/' , (req,res) => res.send('Hello World'))
 app.use(express.static('public'))
 
-app.listen(3000)
+let db;
+
+
+MongoClient.connect(
+  MONGO_URI,
+  (err,database) => {
+    if(err) throw err;
+
+    db = database;
+    app.listen(3000, () => console.log("Listening on port 3000"));
+
+  }
+)
+
+app.get("/data/links",(req,res) => {
+  db.collection("links").find({}).toArray((err,links) => {
+    if(err) throw err;
+    res.json(links);
+  })
+});
