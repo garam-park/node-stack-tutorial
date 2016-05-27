cd8de887c3e1b2948a7959b9e956a926a2f5e53c
garam <netgarng88@gmail.com>
2016-05-27 18:20:08 +0900

    es6 Syntax React

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,12 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-var Hello = React.createClass({
-    render : function() {
+class Hello extends React.Component {
+    render() {
       // return React.createElement("h3", null, 'Hello Webpack!!');
-      return <h3>Hello JSX</h3>
+      return <h3>Hello ES6!!</h3>
     }
-})
+};
 
 // ReactDOM.render(React.createElement(Hello),document.getElementById('react'));
 ReactDOM.render(<Hello/>,document.getElementById('react'));
